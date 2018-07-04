c47bc54f90cdc8838fdf900fbf4ebee0a6682810
zzw <hedgehogloveq@163.com>
2018-07-04 18:09:43 +0800

    First Test

---
+++ JprofileTest/src/test/StyleTest.java
@@ -0,0 +1,15 @@
+
+package test;
+
+public class StyleTest {
+    /**
+     * Constructs a <code>StyleTest</code>
+     * 
+     * @since JprofileTest project_version
+     */
+    StyleTest() {
+    };
+
+    public void SS() {
+    }
+}
